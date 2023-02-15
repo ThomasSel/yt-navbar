4351ef34191516723b71ecd7ea26a451191fb896
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-02-15 12:24:29 +0000

    Move SVG icons to src/assets/icons.js

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 
+const icons = require("../../assets/icons");
+
 const Navbar = (props) => {
   const seed = Math.floor(Math.random() * 1000);
 
@@ -7,13 +9,7 @@ const Navbar = (props) => {
     <div className="fixed top-0 left-0 w-screen h-16 flex items-center justify-between bg-white border-b-gray-300 border-b-[1px] px-4">
       <div className="flex items-center">
         <button className="button">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 48 48"
-            className="w-6 h-6"
-          >
-            <path d="M6 36v-3h36v3Zm0-10.5v-3h36v3ZM6 15v-3h36v3Z" />
-          </svg>
+          <div className="w-6 h-6">{icons.menu}</div>
         </button>
         <a href="/" className="ml-2">
           <img src="/g5614.svg" className="h-6" />
@@ -23,35 +19,17 @@ const Navbar = (props) => {
       <div className="flex">
         <SearchBar />
         <button className="button ml-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 48 48"
-            className="w-6 h-6"
-          >
-            <path d="M24 26.85q-2.15 0-3.6-1.55-1.45-1.55-1.45-3.75V9q0-2.1 1.475-3.55Q21.9 4 24 4t3.575 1.45Q29.05 6.9 29.05 9v12.55q0 2.2-1.45 3.75-1.45 1.55-3.6 1.55Zm0-11.4ZM22.5 42v-6.8q-5.3-.55-8.9-4.45-3.6-3.9-3.6-9.2h3q0 4.55 3.225 7.65Q19.45 32.3 24 32.3q4.55 0 7.775-3.1Q35 26.1 35 21.55h3q0 5.3-3.6 9.2-3.6 3.9-8.9 4.45V42ZM24 23.85q.9 0 1.475-.675.575-.675.575-1.625V9q0-.85-.6-1.425Q24.85 7 24 7t-1.45.575q-.6.575-.6 1.425v12.55q0 .95.575 1.625T24 23.85Z" />
-          </svg>
+          <div className="w-6 h-6">{icons.microphone}</div>
         </button>
       </div>
 
       <div className="flex items-center">
         <button className="button">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 48 48"
-            className="w-6 h-6"
-          >
-            <path d="M18.6 32h3v-6.6h6.6v-3h-6.6v-6.6h-3v6.6H12v3h6.6ZM7 40q-1.2 0-2.1-.9Q4 38.2 4 37V11q0-1.2.9-2.1Q5.8 8 7 8h26q1.2 0 2.1.9.9.9.9 2.1v10.75l8-8v20.5l-8-8V37q0 1.2-.9 2.1-.9.9-2.1.9Zm0-3h26V11H7v26Zm0 0V11v26Z" />
-          </svg>
+          <div className="w-6 h-6">{icons.newVideo}</div>
         </button>
 
         <button className="button">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 48 48"
-            className="w-6 h-6"
-          >
-            <path d="M8 38v-3h4.2V19.7q0-4.2 2.475-7.475Q17.15 8.95 21.2 8.1V6.65q0-1.15.825-1.9T24 4q1.15 0 1.975.75.825.75.825 1.9V8.1q4.05.85 6.55 4.125t2.5 7.475V35H40v3Zm16-14.75ZM24 44q-1.6 0-2.8-1.175Q20 41.65 20 40h8q0 1.65-1.175 2.825Q25.65 44 24 44Zm-8.8-9h17.65V19.7q0-3.7-2.55-6.3-2.55-2.6-6.25-2.6t-6.275 2.6Q15.2 16 15.2 19.7Z" />
-          </svg>
+          <div className="w-6 h-6">{icons.notification}</div>
         </button>
 
         <img
@@ -79,15 +57,8 @@ const SearchBar = (props) => {
     <div className="flex items-center h-10">
       <div className="flex flex-row-reverse items-center h-10 border-[1px] border-gray-300 rounded-l-3xl overflow-hidden focus-within:border-blue-500">
         {search ? (
-          <button className="button mr-2">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 48 48"
-              className="w-6 h-6"
-              onClick={handleCrossButton}
-            >
-              <path d="m12.45 37.65-2.1-2.1L21.9 24 10.35 12.45l2.1-2.1L24 21.9l11.55-11.55 2.1 2.1L26.1 24l11.55 11.55-2.1 2.1L24 26.1Z" />
-            </svg>
+          <button className="button mr-2" onClick={handleCrossButton}>
+            <div className="w-6 h-6">{icons.cross}</div>
           </button>
         ) : null}
         <input
@@ -97,26 +68,16 @@ const SearchBar = (props) => {
           value={search}
           onChange={handleChange}
         ></input>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 48 48"
-          className="w-6 h-6 ml-4 hidden peer-focus:block"
-        >
-          <path d="M39.8 41.95 26.65 28.8q-1.5 1.3-3.5 2.025-2 .725-4.25.725-5.4 0-9.15-3.75T6 18.75q0-5.3 3.75-9.05 3.75-3.75 9.1-3.75 5.3 0 9.025 3.75 3.725 3.75 3.725 9.05 0 2.15-.7 4.15-.7 2-2.1 3.75L42 39.75Zm-20.95-13.4q4.05 0 6.9-2.875Q28.6 22.8 28.6 18.75t-2.85-6.925Q22.9 8.95 18.85 8.95q-4.1 0-6.975 2.875T9 18.75q0 4.05 2.875 6.925t6.975 2.875Z" />
-        </svg>
+        <div className="w-6 h-6 ml-4 hidden peer-focus:block">
+          {icons.magnifyingGlass}
+        </div>
       </div>
       <button
         type="submit"
         className="flex items-center justify-center h-full w-16 rounded-none rounded-r-3xl border-[1px] border-gray-300 border-l-0 bg-gray-50 hover:bg-gray-100
             hover:border-gray-300"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 48 48"
-          className="w-6 h-6"
-        >
-          <path d="M39.8 41.95 26.65 28.8q-1.5 1.3-3.5 2.025-2 .725-4.25.725-5.4 0-9.15-3.75T6 18.75q0-5.3 3.75-9.05 3.75-3.75 9.1-3.75 5.3 0 9.025 3.75 3.725 3.75 3.725 9.05 0 2.15-.7 4.15-.7 2-2.1 3.75L42 39.75Zm-20.95-13.4q4.05 0 6.9-2.875Q28.6 22.8 28.6 18.75t-2.85-6.925Q22.9 8.95 18.85 8.95q-4.1 0-6.975 2.875T9 18.75q0 4.05 2.875 6.925t6.975 2.875Z" />
-        </svg>
+        <div className="w-6 h-6">{icons.magnifyingGlass}</div>
       </button>
     </div>
   );
