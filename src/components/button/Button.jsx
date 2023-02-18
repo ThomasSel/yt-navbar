fe9104e3b632ed0ed2b805885bfeb0a633a7fda6
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-02-18 17:32:27 +0000

    Add tooltip to the button components
    NOTE:
    - We need to make sure the tooltip stays on one line (eg for
    the mic)
    - We need to add a tooltip to the search button (not a Button
    component)
    - We need to make sure nothing shows up when there is no tooltip

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,8 +1,13 @@
 const Button = (props) => {
   return (
-    <button className="button" onClick={props.handleClick}>
-      <div className="w-6 h-6">{props.icon}</div>
-    </button>
+    <div className="flex justify-center relative mx-1 first:ml-0 last:mr-0">
+      <button className="button peer" onClick={props.handleClick}>
+        <div className="w-6 h-6">{props.icon}</div>
+      </button>
+      <div className="button-tooltip invisible peer-hover:visible">
+        {props.tooltip}
+      </div>
+    </div>
   );
 };
 
