2fd757ce80efcb74f0434fccda8345fcf4468b72
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-02-18 18:57:13 +0000

    Add basic history dropdown

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -5,6 +5,7 @@ import Button from "../button/Button";
 
 const SearchBar = (props) => {
   const [search, setSearch] = useState("");
+  const [history, setHistory] = useState([]);
 
   const handleChange = (event) => {
     setSearch(event.target.value);
@@ -15,9 +16,14 @@ const SearchBar = (props) => {
     setSearch("");
   };
 
+  const handleSearchButton = (event) => {
+    event.preventDefault();
+    setHistory([search, ...history]);
+  };
+
   return (
-    <div className="flex items-center h-10">
-      <div className="flex flex-row-reverse items-center h-10 border-[1px] border-gray-300 rounded-l-3xl overflow-hidden focus-within:border-blue-500">
+    <div className="flex items-center h-10 relative">
+      <div className="flex flex-row-reverse items-center h-10 border-[1px] border-gray-300 rounded-l-3xl overflow-hidden focus-within:border-blue-500 peer">
         <div className="flex w-96 peer">
           <input
             type="text"
@@ -36,10 +42,20 @@ const SearchBar = (props) => {
           {icons.magnifyingGlass}
         </div>
       </div>
+
+      <ul className="absolute left-0 top-full py-2 bg-white rounded-2xl overflow-hidden drop-shadow-md invisible peer-focus-within:visible">
+        {history.map((search) => (
+          <li className="flex items-center hover:bg-gray-200 py-1">
+            <div className="w-6 h-6 ml-4">{icons.history}</div>
+            <div className="w-96 pl-4 text-lg font-semibold">{search}</div>
+          </li>
+        ))}
+      </ul>
+
       <div className="flex justify-center relative h-full w-16">
         <button
-          className="flex items-center justify-center h-full w-16 rounded-none rounded-r-3xl border-[1px] border-gray-300 border-l-0 bg-gray-50 hover:bg-gray-100
-              hover:border-gray-300 peer"
+          className="flex items-center justify-center h-full w-16 rounded-none rounded-r-3xl border-[1px] border-gray-300 border-l-0 bg-gray-50 hover:bg-gray-100 hover:border-gray-300 peer"
+          onClick={handleSearchButton}
         >
           <div className="w-6 h-6">{icons.magnifyingGlass}</div>
         </button>
