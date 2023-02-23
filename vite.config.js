6cddce73d5489bdec3f20bcc24c3836f09c7a3bf
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-02-23 16:35:06 +0000

    Use environment to set hosting port

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    port: parseInt(process.env.VITE_PORT || 8000),
+    strictPort: true,
+  }
 })
